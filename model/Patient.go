57e3c41e182e38f1fe565a8a90da2c2926eb90e8
มาตุภูมิ ใครบุตร <yeawyow@Macintosh.local>
2022-12-02 10:53:12 +0700

    ddd

--- model/Patient.go
+++ model/Patient.go
@@ -5,11 +5,12 @@ import (
 )
 
 type Patient struct {
-	HosGuid  string
-	Hn       int `gorm:"primaryKey"`
-	Pname    string
+	HosGuid  string `json:"fname" binding:"required"`
+	Hn       string `json:"hn" gorm:"primaryKey"`
+	Pname    string `json:"pname" binding:"required"`
 	Fname    string `json:"fname" binding:"required"`
-	Lname    string
+	Lname    string `json:"lname" binding:"required"`
+	MobilePhoneNumber string `json:"phonnumber"`
 	Birthday time.Time
 	Cid string `json:"cid"`
 
